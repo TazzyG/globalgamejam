9eaf101ca0c6bb1b665737cc6cb3d503eec9ed9e
Laurie Gloge <gloge@haathi-consulting.com>
2018-01-27 18:25:09 -0700

    death velocity set up

--- game.rb
+++ game.rb
@@ -7,6 +7,8 @@ JUMP_VEL = Vec[0, -50]
 OBSTACLE_SPEED = 200 # pixels/s
 OBSTACLE_SPAWN_INTERVAL = 2 #seconds
 OBSTACLE_GAP = 100 # pixels
+DEATH_VELOCITY = Vec[50, -500] #pixes/s
+DEATH_ROTATIONAL_VEL = 360 # degrees/s
 
 Rect = DefStruct.new{{
 	pos: Vec[0, 0], #x, y
@@ -25,6 +27,7 @@ GameState = DefStruct.new{{
 	scroll_x: 0,
 	player_pos: Vec[20,0],
 	player_vel: Vec[0,0],
+	player_rotation: 0, 
 	obstacles: [], # array of Obstacles
 	obstacle_countdown: OBSTACLE_SPAWN_INTERVAL,
 	}}
@@ -50,7 +53,7 @@ class GameWindow < Gosu::Window
 	def button_down(button)
     case button
     when Gosu::KbEscape then close
-    when Gosu::KbSpace then @state.player_vel.set!(JUMP_VEL)
+    when Gosu::KbSpace then @state.player_vel.set!(JUMP_VEL) if @state.alive
     end
   end
 
@@ -81,8 +84,13 @@ class GameWindow < Gosu::Window
     end
   
 
-	  if player_is_colliding? 
+	  if @state.alive && player_is_colliding? 
 	  	@state.alive = false
+	  	@state.player_vel.set!(DEATH_VELOCITY)
+	  end
+
+	  unless @state.alive 
+	  	@state.player_rotation += dt*DEATH_ROTATIONAL_VEL
 	  end
 	end
 
@@ -133,7 +141,9 @@ class GameWindow < Gosu::Window
 
     end
 	
-		@images[:player].draw(@state.player_pos.x, @state.player_pos.y, 0)
+		@images[:player].draw_rot(
+			@state.player_pos.x, @state.player_pos.y, 
+			0, @state.player_rotation, 0, 0)
 
 		debug_draw
 	end
