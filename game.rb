bc96a83ece5a26dd84ff708702e2ac51a03c6e50
Laurie Gloge <gloge@haathi-consulting.com>
2018-01-28 11:24:33 -0700

    added sound, still buggy

--- game.rb
+++ game.rb
@@ -1,15 +1,34 @@
 require 'gosu'
 require_relative 'defstruct'
 require_relative 'vector'
+require_relative 'timer'
+require_relative 'animation'
 
-PLAYER_ANIMATION_FPS = 5 # frames/second
+PLAYER_ANIMATION_FPS = 5.0 # frames/second
 GRAVITY = Vec[0, 50] # pixels/s^2
 JUMP_VEL = Vec[0, -50] # pixel/s
-OBSTACLE_SPEED = 200 # pixel/s
-OBSTACLE_SPAWN_INTERVAL = 1.3 #seconds
-OBSTACLE_GAP = 300 #pixels
 DEATH_VELOCITY = Vec[50, -500] # pixels/s
 DEATH_ROTATIONAL_VEL = 360 # degrees/s
+RESTART_INTERVAL = 3 #seconds
+PLAYER_FRAMES = [:player, :player1]
+OBSTACLE_PADDING = 150 #px
+DIFFICULTIES = {
+  easy: {
+    speed: 150, # pixels/s
+    obstacle_gap: 220, # pixels
+    obstacle_spawn_interval: 2.0, # secs
+  },
+  medium: {
+    speed: 200, # pixels/s
+    obstacle_gap: 180, #pixels
+    obstacle_spawn_interval: 1.3, #seconds
+  },
+  hard: {
+    speed: 400, # pixels/s
+    obstacle_gap: 160, # pixels
+    obstacle_spawn_interval: 1.0, #seconds
+  },
+}
 
 Rect = DefStruct.new{{
   pos: Vec[0,0], #x, y
@@ -25,9 +44,20 @@ end
 Obstacle = DefStruct.new{{
   pos: Vec[0,0],
   player_has_crossed: false,
+  gap: DIFFICULTIES[:easy][:obstacle_gap],
 }}
 
+Particle = DefStruct.new{{
+  pos: Vec[0, 0],
+  velocity: Vec[0.0],
+  rotation: 0,
+  rotational_velocity: 0,
+  scale: 1.0, 
+  tint: Gosu::Color::WHITE,
+  }}
+
 GameState = DefStruct.new{{
+  difficulty: :medium,
   score: 0,
   started: false,
   alive: true,
@@ -35,15 +65,20 @@ GameState = DefStruct.new{{
   player_pos: Vec[20,250],
   player_vel: Vec[0,0],
   player_rotation: 0,
+  player_animation: Animation.new(PLAYER_ANIMATION_FPS, PLAYER_FRAMES),
   player_frame: 0, 
-  player_frame_remaining: 1/PLAYER_ANIMATION_FPS,
+  player_frame_remaining: 1.0/PLAYER_ANIMATION_FPS,
   obstacles: [], # array of Obstacle
-  obstacle_countdown: OBSTACLE_SPAWN_INTERVAL,
+  # obstacle_countdown: OBSTACLE_SPAWN_INTERVAL,
+  particles: [],
+  obstacle_timer: Timer::Looping.new(DIFFICULTIES[:medium][:obstacle_spawn_interval]),
+  restart_time: Timer::OneShot.new(RESTART_INTERVAL),
 }}
 
-PLAYER_FRAMES = [:player, :player1]
+
 
 class GameWindow < Gosu::Window
+  # SAVE_PATH = ENV['HOME'] + '/.ggj_save'
   def initialize(*args)
     super
     @font = Gosu::Font.new(self, Gosu.default_font_name, 40)
@@ -56,45 +91,91 @@ class GameWindow < Gosu::Window
 			player: Gosu::Image.new(self, 'images/nemo_forward.png', false),
       player1: Gosu::Image.new(self, 'images/nemo.png', false),
 			obstacle: Gosu::Image.new(self, 'images/yellow.png', false),
+      particle: Gosu::Image.new(self, 'images/soundwaves.png', false),
 			# golf: Gosu::Image.new(self, 'images/golf.jpg', false),
 			# trump: Gosu::Image.new(self, 'images/trump.png', false),
+
 		}
+
+    @sounds = {
+      flap: Gosu::Sample.new(self, 'audio/bubble.wav'),
+      score: Gosu::Sample.new(self, 'audio/score.ogg'),
+    }
     @state = GameState.new
+
+    @music = Gosu::Song.new(self, 'sound/Miaow-07-Bubble.mp3')
+    @music.play
   end
 
   def button_down(button)
     case button
     when Gosu::KbEscape then close
+    when Gosu::Kb1 then set_difficulty(:easy)
+    when Gosu::Kb2 then set_difficulty(:medium)
+    when Gosu::Kb3 then set_difficulty(:hard)
     when Gosu::KbSpace
-      @state.player_vel.set!(JUMP_VEL) if @state.alive
+      if @state.alive
+        @state.player_vel.set!(JUMP_VEL) if @state.alive
+        @sounds[:flap].play(0.2, rand(0.9..1.1))
+      end
       @state.started = true
     end
+
+  end
+  def set_difficulty(name)
+    @state.difficulty = name
+    @state.obstacle_timer.interval = DIFFICULTIES[name][:obstacle_spawn_interval]
+  end
+
+  def save_game
+    File.binwrite(SAVE_PATH, Marshal.dump(@state))
+  end
+
+  def load_game
+    @state = Marshal.load(File.binread(SAVE_PATH))
   end
 
   def update
     dt = update_interval / 1000.0
 
-    @state.scroll_x += dt*OBSTACLE_SPEED*0.5
+    @state.scroll_x += dt*difficulty[:speed]*0.5
     if @state.scroll_x > @images[:foreground].width
       @state.scroll_x = 0
     end
+
+    @state.player_animation.update(dt)
+
+    @state.particles.each do |part|
+      part.velocity += dt*GRAVITY
+      part.pos += dt*part.velocity
+      part.rotation += dt*part.rotational_velocity
+    end
+    @state.particles.reject! { |parts| parts.pos.y >= height }
 
     return unless @state.started
 
     @state.player_vel += dt*GRAVITY
     @state.player_pos += dt*@state.player_vel
 
-    @state.obstacle_countdown -= dt
-    if @state.obstacle_countdown <= 0
-      @state.obstacles << Obstacle.new(pos: Vec[width, rand(50..320)])
-      @state.obstacle_countdown += OBSTACLE_SPAWN_INTERVAL
+    if @state.alive
+      
+      @state.obstacle_timer.update(dt) do
+        gap =  difficulty[:obstacle_gap]
+        lower_bound = height - OBSTACLE_PADDING - gap
+        @state.obstacles << Obstacle.new(
+          pos: Vec[width, rand(OBSTACLE_PADDING..lower_bound)],
+          gap: gap,
+          )
+      end
     end
 
     @state.obstacles.each do |obst|
       obst.pos.x -= dt*OBSTACLE_SPEED
       if obst.pos.x < @state.player_pos.x && !obst.player_has_crossed && @state.alive
         @state.score += 1
+        @sounds[:score].play(0.15, 0.8 + @state.score * 0.05)
         obst.player_has_crossed = true
+        particle_burst
       end
     end
     @state.obstacles.reject! { |obst| obst.pos.x < -@images[:obstacle].width }
@@ -106,7 +187,32 @@ class GameWindow < Gosu::Window
 
     unless @state.alive
       @state.player_rotation += dt*DEATH_ROTATIONAL_VEL
+      @state.restart_timer.update(dt) { restart_game }
     end
+  end
+
+  def particle_burst
+    30.times do
+      @state.particles << Particle.new(
+        pos: Vec[width/2.0, 60],
+        velocity: Vec[rand(-100..100), rand(-300..-10)],
+        rotation: rand(0..360),
+        rotational_velocity: rand(-360..360),
+        scale: rand(0.5..1.0),
+        tint: Gosu::Color.new(
+          255,
+          rand(150..255),
+          rand(150..255),
+          rand(150..255),
+        ),
+      )
+    end
+  end
+
+  def restart_game
+    old_difficulty = @state.difficulty
+    @state = GameState.new(scroll_x: @state.scroll_x)
+    set_difficulty(old_difficulty)
   end
 
   def player_is_colliding?
@@ -126,6 +232,15 @@ class GameWindow < Gosu::Window
 
   def draw
     @images[:background].draw(0, 0, 0)
+    @state.particles.each do |part|
+      @images[:particle].draw_rot(
+        part.pos.x, part.pos.y, 0,
+        part.rotation,
+        0.5, 0.5,
+        part.scale, part.scale,
+        part.tint,
+      )
+    end
     @images[:foreground].draw(-@state.scroll_x, 0, 0)
     @images[:foreground].draw(-@state.scroll_x + @images[:foreground].width, 0, 0)
 
@@ -144,12 +259,17 @@ class GameWindow < Gosu::Window
       0, @state.player_rotation,
       0, 0)
 
-    @font.draw_rel(@state.score.to_s, width/2.0, 60, 0, 0.5, 0.5) 
+    @font.draw_rel(@state.score.to_s, width/2.0, 60, 0, 0.5, 0.5)
+    @font.draw_rel(@state.difficulty.to_s, width - 10, height - 10, 0, 1.0, 1.0) 
     #debug_draw
   end
 
+  def difficulty
+    DIFFICULTIES[@state.difficulty]
+  end
+
   def player_frame
-    @images[PLAYER_FRAMES[@state.player_frame]]
+     @images[@state.player_animation.frame]
   end
 
   def player_rect
